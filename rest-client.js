201b561af4c4679c5f5b6657ffa8b0157b5f31b2
Gene Gotimer <otherdevopsgene@portinfo.com>
2024-02-12 13:50:55 -0500

    feat: mock exercise

---
+++ rest-client.js
@@ -0,0 +1,28 @@
+"use strict";
+
+export class RestClient {
+  constructor(url, opts = {}) {
+    this.url = url;
+    this.options = opts;
+  }
+
+  // Dummy method stub
+  get(id) {
+    return {status: 501};
+  }
+
+  // Dummy method stub
+  put(obj) {
+    return {status: 501};
+  }
+
+  // Dummy method stub
+  post(obj) {
+    return {status: 501};
+  }
+
+  // Dummy method stub
+  delete(id) {
+    return {status: 501};
+  }
+}
